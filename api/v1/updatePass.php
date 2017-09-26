13d451b8824c5a1d404afcc4c38572f4e9d05e4d
Arvind <balgo_arvind@hotmail.com>
2017-09-26 07:00:24 +0400

    update password script

--- api/v1/updatePass.php
+++ api/v1/updatePass.php
@@ -5,37 +5,42 @@
  * Date: 19/07/2017
  * Time: 04:12
  */
+include_once 'include_all.php';
 require 'authFN.php';
 require_once '../classes/passwordHash.php';
+
 if (!isset($_SESSION)) {
     session_start();
 }
-if($_SESSION["uid"] == "") {
+
+if ($_SESSION["uid"] == "") {
     print json_encode(0);
     return;
 }
-if($_POST["password"] == "" || $_POST["password1"] || $_POST["password2"] == "") {
+
+if ($_POST["password"] == "" || $_POST["password1"] == "" || $_POST["password2"] == "") {
     print json_encode(0);
     return;
 }
 
 $db = new DbHandler();
-$user = $db->getOneRecord("select uid,name,password,email,created,admin, pays, surname, city, postalcode,phone, address, admintype  from customers_auth where uid=".$_SESSION["uid"]);
-if($user != NULL) {
-    if(passwordHash::check_password($user['password'],$_POST["password"])){
+$user = $db->getOneRecord("select uid,name,password,email,created,admin, pays, surname, city, postalcode,phone, address, admintype  from customers_auth where admin=0 and uid=" . $_SESSION["uid"]);
+
+if ($user != NULL) {
+    if (passwordHash::check_password($user['password'], $_POST["password"])) {
         $user = new users();
         $user = $user->findByPrimaryKey($_SESSION["uid"]);
-        if($user) {
-            $user->setPassword( passwordHash::hash($password));
+        if ($user) {
+            $user->setPassword(passwordHash::hash($_POST["password2"]));
             $user->save();
         }
-    }
-    else {
+        print json_encode(1);
+        return;
+    } else {
         print json_encode(0);
         return;
     }
-}
-else {
+} else {
     print json_encode(0);
     return;
 }
